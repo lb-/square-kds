8ba18a41e736d448ac23cf811e4fb2db9f2788e9
LB (Ben) Johnston <github@lb.ee>
2015-04-20 09:19:07 +1000

    Only show last 30mins of tickets

--- server/publications.js
+++ server/publications.js
@@ -4,9 +4,15 @@ Meteor.publish('payments', function (options) {
   options = options || {sort: {created_at: -1}};
   var fields = {
     itemizations: 1,
-    // receipt_url: 1,
+    tender: 1,
     created_at: 1,
     cleared: 1,
   };
-  return db.Payments.find({}, {fields: fields});
+  //db.Payments.find().fetch()
+  var query = {
+    created_at: {
+      $gte: moment().subtract(30,'minutes').toISOString()
+    }
+  };
+  return db.Payments.find(query, {fields: fields});
 });
